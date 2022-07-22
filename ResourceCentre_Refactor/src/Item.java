bc312a133d8150f0ef883544e786131e60a6887c
21027430 <21027430@LAPTOP-SMM80LVH.lan>
2022-07-22 14:49:40 +0800

    first push to L10 repository in github

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -14,7 +14,23 @@ public class Item {
 	public String toString() {
 		
 		// Write your codes here
+		String itemInfo = String.format("%-10s %-30s %-10s %-10s", 
+				assetTag,
+				description, 
+				showAvailability(isAvailable),
+				dueDate);
 		return null;
+	}
+	
+	public String showAvailability(boolean isAvailable) {
+		String avail;
+
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
 	}
 	public String getAssetTag() {
 		return assetTag;
